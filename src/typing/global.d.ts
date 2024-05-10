beede2aceed458d22a048dcaf15250274520dcf6
qianyeyixi <1521341740@qq.com>
2024-05-10 17:46:40 +0800

    feat: 更新全局类型定义和添加本地存储模块
    为了支持ant-design-vue库的类型检查，更新了tsconfig.json的类型声明列表，并添加了ant-design-vue/typings/global。
    还添加了本地存储模块和会话存储模块，以增强代码的可读性和维护性。

--- src/typing/global.d.ts
+++ src/typing/global.d.ts
@@ -1,8 +1,5 @@
 interface Window {
-  $loadingBar?: import('naive-ui').LoadingBarProviderInst;
-  $dialog?: import('naive-ui').DialogProviderInst;
-  $message?: import('naive-ui').MessageProviderInst;
-  $notification?: import('naive-ui').NotificationProviderInst;
+
 }
 
 interface ViewTransition {
